e66b1619ff54996124cd6cc17d3ef7a73a4feec7
alexhkz <alexwebcode@gmail.com>
2022-01-31 18:17:45 +0300

    change xmlhttprequest to fetch

--- src/js/main.js
+++ src/js/main.js
@@ -246,11 +246,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			`;
 			form.insertAdjacentElement('afterend', statusMessage); // показывает сообщение на странице
 			
-
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');  // метод и путь на который ссылаемся
-
-			request.setRequestHeader('Content-type', 'application/json');
 			const formData = new FormData(form); // создаём формдату с данными из инпутов
 
 			const object = {}; // переберём данные из формы и поместив в object
@@ -258,19 +253,22 @@ window.addEventListener('DOMContentLoaded', () => {
 				object[key] = value;
 			});
 
-			const json = JSON.stringify(object); // конвертируем данные в json формат
-
-			request.send(json); // отправляем данные из инпутов
-
-			request.addEventListener('load', () => { // когда запрос выполнился
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success); // оповещаем пользователя об успехе
-					form.reset(); // сбрасывем форму
-					statusMessage.remove(); // удаляем блок с сообщением со страницы
-				} else {
-					showThanksModal(message.failure); // при ошибке 
-				}
+			fetch('server1.php', {
+				method: "POST",
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(object)
+			})
+			.then(data => data.text())
+			.then(data => {
+				console.log(data);
+				showThanksModal(message.success); // оповещаем пользователя об успехе
+				statusMessage.remove(); // удаляем блок с сообщением со страницы
+			}).catch(() => {
+				showThanksModal(message.failure); // при ошибке 
+			}).finally(() => {
+				form.reset(); // сбрасывем форму
 			});
 		});
 	}
